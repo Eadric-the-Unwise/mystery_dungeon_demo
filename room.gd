8c8e0a50b0231d0984e2f13800a549baf0085266
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2024-02-07 08:31:01 -0800

    add room.gd. EOD

---
+++ room.gd
@@ -0,0 +1,13 @@
+# ?Where is this Room class referenced?
+class_name Room
+extends Area2D
+
+@onready var camera_2d: Camera2D = $"../Camera2D"
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Entered: ", name)
+	camera_2d.position = position
+
+func _on_area_exited(area: Area2D) -> void:
+	print("Exited: ", name)
