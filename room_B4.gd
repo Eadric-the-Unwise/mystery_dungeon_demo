c68ef68fc1cf7036649a84c9c64d8df2052deed2
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2024-05-22 11:48:50 -0700

    add spawn_map to each room

--- room_B4.gd
+++ room_B4.gd
@@ -3,7 +3,7 @@ extends Room
 var goblin_enemy := preload("res://enemy.tscn")
 # Add an array for enemies for this room
 var room_enemies: Array = [
-	goblin_enemy, goblin_enemy
+	goblin_enemy
 ]
 var enemy_x = 208
 var enemy_y = 16
