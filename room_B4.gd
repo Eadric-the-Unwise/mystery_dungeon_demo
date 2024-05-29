31d5b5dfd7e51a7ce71eb48016462a1a276c30ff
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2024-05-29 06:57:03 -0700

    renamed 'enemy' to 'goblin_regular'

--- room_B4.gd
+++ room_B4.gd
@@ -1,7 +1,7 @@
 extends Room
 #-----------------------------------------
 @onready var spawn_map = $"Spawn Map"
-var goblin_enemy := preload("res://enemy.tscn")
+var goblin_enemy := preload("res://goblin_regular.tscn")
 # Add an array for enemies for this room
 var spawn_enemies: Array = [
 	goblin_enemy
